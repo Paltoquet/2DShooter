c487b07d575f1001b06c38be0f69fa5a9a5450d0
Paltoquet <thibault.ober@gmail.com>
2019-09-28 21:00:47 +0200

    Add a dummy cameraController

---
+++ Assets/Animation/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private float m_currentSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_currentSize = GetComponent<UnityEngine.Camera>().orthographicSize; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
+    }
+}
