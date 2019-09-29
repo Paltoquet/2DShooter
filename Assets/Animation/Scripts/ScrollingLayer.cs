6fb5d87ecc471ed3cf6e54fbbf21ab473d536d87
Paltoquet <thibault.ober@gmail.com>
2019-09-29 19:01:30 +0200

    Add parallax background

---
+++ Assets/Animation/Scripts/ScrollingLayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingLayer : MonoBehaviour
+{
+
+    //try to apply perspective distorsion 
+    //If you apply a displacement Delta on a object close to the camera at distance d
+    //The Layer which is at a distance d' of the camera should be applied a displacement Delta'
+    //Delta' = Delta * d'/d
+    //Now this displacement should be scaled to the actual size of the layer 
+    //If the local size of the image is l but in the world its L
+    //The displacement become Delta' * l / L
+    //displacementRatio becomes : d' / d * l / L
+
+    public float displacementRatio;
+
+    public void MoveLayer(Vector2 cameraMovement)
+    {
+        Vector2 displacement = cameraMovement * displacementRatio;
+        Vector2 translation = displacement - cameraMovement;
+        this.gameObject.transform.Translate(new Vector3(translation.x, translation.y, 0.0f));
+    }
+}
---
+++ Assets/Animation/Scripts/ScrollingManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingManager : MonoBehaviour
+{
+
+    public List<ScrollingLayer> m_layers;
+    public Camera m_camera;
+
+    private Vector3 m_oldCameraPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_oldCameraPosition = m_camera.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 updatedCameraPosition = m_camera.transform.position;
+        Vector2 displacement = updatedCameraPosition - m_oldCameraPosition;
+        if (displacement != Vector2.zero) {
+            foreach (var layer in m_layers)
+            {
+                layer.MoveLayer(displacement);
+            }
+            m_oldCameraPosition = updatedCameraPosition;
+        }
+    }
+}
