52302a13e71d19539ce9fc9ad20d2496da437be7
Paltoquet <thibault.ober@gmail.com>
2020-07-13 23:05:17 +0200

    add level navigation

---
+++ Assets/Animation/Scripts/Context/LevelNavigationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelNavigationController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private string m_mainMenuSceneName;
+
+    void Start()
+    {
+        m_mainMenuSceneName = "Scenes/LoginScreen"; 
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.keyCode == KeyCode.Escape)
+        {
+            SceneManager.LoadScene(m_mainMenuSceneName, LoadSceneMode.Single);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Animation/Scripts/MainMenu/MainUiController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainUiController : MonoBehaviour
+{
+
+    public Button arenaButton;
+    public Button characterCreationButton;
+
+    private string m_arenaSceneName;
+    private string m_characterCreationSceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_arenaSceneName = "Scenes/Main";
+        m_characterCreationSceneName = "Scenes/CharacterCreation";
+
+        arenaButton.onClick.AddListener(startArena);
+        characterCreationButton.onClick.AddListener(startCharacterCreation);
+    }
+
+
+    void startArena()
+    {
+        SceneManager.LoadScene(m_arenaSceneName, LoadSceneMode.Single);
+    }
+
+    void startCharacterCreation()
+    {
+        SceneManager.LoadScene(m_characterCreationSceneName, LoadSceneMode.Single);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
